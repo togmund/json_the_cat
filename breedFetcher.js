cc5397f0b2b72c0f407ac2c123a3608f7adb1bae
Ted Ogmundson <togmund@gmail.com>
2019-10-24 20:50:00 +0000

    updated request format to receive cli arguments and fail elegantly in the case of errors and non-200 status codes.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,24 @@
+// Outside Dependencies
 const request = require('request');
+const breedSearchEndpoint = `https://api.thecatapi.com/v1/breeds/search?q=`;
+const cliSearchTerm = process.argv.slice(2,3);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, response, body) => {
+// Function Calls & Rendering new state
+request(`${breedSearchEndpoint}${cliSearchTerm}`, (error, response, body) => {
   // Response Types
-  console.log(`Typeof body: ${typeof body}`);
-  console.log(`Typeof error: ${typeof error}`);
-  console.log(`Typeof response: ${typeof response}`);
-  console.log(`Typeof status code: ${typeof response.statusCode}`);
-
+  // console.log(`Typeof body: ${typeof body}`);
+  // console.log(`Typeof error: ${typeof error}`);
+  // console.log(`Typeof response: ${typeof response}`);
+  // console.log(`Typeof status code: ${typeof response.statusCode}`);
+  if (error)  {
+    throw error;
+  }
+  if (response !== 200) {
+    return `Expected "Status Code: 200", received "Status Code:${response.statusCode}"\r\nTry another search term.`
+  }
   // Parsed body
   const data = JSON.parse(body);
 
   // Human Readable output
   console.log(`Description:\n${data[0].description}`);
 });
-
-
