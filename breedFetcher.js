c276d94a6e3990f14281120a68c64d821199dcfb
Ted Ogmundson <togmund@gmail.com>
2019-10-24 20:36:36 +0000

    parsing the body of the response

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,14 @@
 const request = require('request');
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, response, body) => {
-  console.log(typeof body);
   console.log(typeof error);
   console.log(typeof response);
   console.log(typeof response.statusCode);
+
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+
 });
 
 
